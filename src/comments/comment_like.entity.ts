a54fa7867c47d0a9690a246c9111b78bba7fc562
DANG-PH <dangph.ptit@gmail.com>
2026-04-14 17:50:31 +0700

    fix: Unique indexing

--- src/comments/comment_like.entity.ts
+++ src/comments/comment_like.entity.ts
@@ -2,7 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, Unique, Index
 
 // Leftprefix k có tác dụng trong case này nên để cái nào trước cũng được
 // leftprefix quan trọng khi có case 1 trong 2 cái k dc query mà cái còn lại thì có
-Unique(['commentId', 'userId'])
+@Unique(['commentId', 'userId'])
 @Entity('comment_likes')
 export class CommentLikeEntity {
   @PrimaryGeneratedColumn()
